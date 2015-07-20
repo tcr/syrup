1f47ade5e0f574177a59d5cff6d03e9c3afab003
Tim Cameron Ryan <id@timryan.org>
2015-07-19 18:42:31 -0700

    Cleans up context loading code.

--- syrup.js
+++ syrup.js
@@ -337,326 +337,327 @@ var DefaultContext = function() {
     return FlowControl;
 
   })();
-  return new exports.Context({
-    'eval': function(call) {
-      return this["eval"].apply(this, call);
-    },
 
-    'fn': macro(function() {
-      var arg, args, ctx, defaults, f, i, stats;
-      args = arguments[0], stats = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-      if ((args != null ? args[0] : void 0) !== 'list') {
-        throw new Error('Parse error: function arguments must be list');
-      }
-      defaults = [];
-      args = (function() {
-        var j, len, ref, results;
-        ref = args.slice(1);
-        results = [];
-        for (i = j = 0, len = ref.length; j < len; i = ++j) {
-          arg = ref[i];
-          if (typeof arg !== 'string') {
-            if (Array.isArray(arg) && arg[0] === '=' && arg.length === 3 && typeof arg[1] === 'string') {
-              defaults[i] = this["eval"](arg[2]);
-              results.push(arg[1]);
-            } else {
-              throw new Error('Parse error: invalid function argument definition');
-            }
-          } else {
-            results.push(arg);
-          }
-        }
-        return results;
-      }).call(this);
-      ctx = this;
-      f = function() {
-        var ctx2, j, len, ref, stat, vals;
-        vals = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-        ctx2 = new exports.Context(ctx.vars);
-        for (i = j = 0, len = args.length; j < len; i = ++j) {
-          arg = args[i];
-          ctx2.vars[arg] = (ref = vals[i]) != null ? ref : defaults[i];
-        }
-        vals = (function() {
-          var len1, n, results;
-          results = [];
-          for (n = 0, len1 = stats.length; n < len1; n++) {
-            stat = stats[n];
-            results.push(ctx2["eval"](stat));
-          }
-          return results;
-        })();
-        return vals[vals.length - 1];
-      };
-      f.parameters = args;
-      return f;
-    }),
+  var host = {}
 
-    'do': function(fn) {
-      var arg;
-      return fn.apply(null, (function() {
-        var j, len, ref, results;
-        ref = fn.parameters || [];
-        results = [];
-        for (j = 0, len = ref.length; j < len; j++) {
-          arg = ref[j];
-          results.push(this.vals[arg]);
-        }
-        return results;
-      }).call(this));
-    },
+  host.host = host;
 
-    'macro': macro(function() {
-      var args, f, m, stats;
-      args = arguments[0], stats = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-      f = this["eval"](['fn', args].concat(slice.call(stats)));
-      m = macro(function() {
-        var args;
-        args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-        return this["eval"](f.apply(null, args));
-      });
-      return m;
-    }),
+  host.eval = function (call) {
+    return this["eval"].apply(this, call);
+  };
 
-    'call': function() {
-      var args, meth, obj;
-      obj = arguments[0], meth = arguments[1], args = 3 <= arguments.length ? slice.call(arguments, 2) : [];
-      return obj[meth].apply(obj, args);
-    },
-
-    'quote': macro(function(arg) {
-      return arg;
-    }),
-
-    'atom?': macro(function(arg) {
-      return typeof arg === 'string';
-    }),
-
-    'list': function() {
-      var args;
-      args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-      return args;
-    },
-
-    'pairs': function(obj) {
-      var k, results, v;
+  host.fn = macro(function() {
+    var arg, args, ctx, defaults, f, i, stats;
+    args = arguments[0], stats = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    if ((args != null ? args[0] : void 0) !== 'list') {
+      throw new Error('Parse error: function arguments must be list');
+    }
+    defaults = [];
+    args = (function() {
+      var j, len, ref, results;
+      ref = args.slice(1);
       results = [];
-      for (k in obj) {
-        v = obj[k];
-        results.push([k, v]);
+      for (i = j = 0, len = ref.length; j < len; i = ++j) {
+        arg = ref[i];
+        if (typeof arg !== 'string') {
+          if (Array.isArray(arg) && arg[0] === '=' && arg.length === 3 && typeof arg[1] === 'string') {
+            defaults[i] = this["eval"](arg[2]);
+            results.push(arg[1]);
+          } else {
+            throw new Error('Parse error: invalid function argument definition');
+          }
+        } else {
+          results.push(arg);
+        }
       }
       return results;
-    },
-
-    'first': function(list) {
-      return list != null ? list[0] : void 0;
-    },
-
-    'rest': function(list) {
-      return list != null ? list.slice(1) : void 0;
-    },
-
-    'concat': function() {
-      var args, list;
-      list = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-      return list.concat(args);
-    },
-
-    'empty?': function(list) {
-      return !(list != null ? list.length : void 0);
-    },
-
-    'new': function() {
-      var Obj, args;
-      Obj = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-      return (function(func, args, ctor) {
-        ctor.prototype = func.prototype;
-        var child = new ctor, result = func.apply(child, args);
-        return Object(result) === result ? result : child;
-      })(Obj, args, function(){});
-    },
-
-    '=': macro(function(left, v) {
-      var assign, tryAssign;
-      assign = (function(_this) {
-        return function(name, val) {
-          if (hasOwnProperty(_this.vars, name)) {
-            throw new Error('Cannot reassign variable in this scope: ' + name);
-          }
-          return _this.vars[name] = val;
-        };
-      })(this);
-      tryAssign = (function(_this) {
-        return function(left, v) {
-          var i, j, len, ref, results, x;
-          if (Array.isArray(left) && left[0] === 'list') {
-            ref = left.slice(1);
-            results = [];
-            for (i = j = 0, len = ref.length; j < len; i = ++j) {
-              x = ref[i];
-              results.push(tryAssign(x, v[i]));
-            }
-            return results;
-          } else if (typeof left === 'string') {
-            return assign(left, v);
-          } else {
-            throw new Error('Invalid assignment left hand side');
-          }
-        };
-      })(this);
-      return tryAssign(left, this["eval"](v));
-    }),
-
-    // Arithmetic
-
-    '==': function(a, b) {
-      return a === b;
-    },
-
-    '+': function(a, b) {
-      return a + b;
-    },
-
-    '-': function(a, b) {
-      return a - b;
-    },
-
-    '/': function(a, b) {
-      return a / b;
-    },
-
-    '*': function(a, b) {
-      return a * b;
-    },
-
-    '%': function(a, b) {
-      return a % b;
-    },
-
-    '.': function(a, b) {
-      return a[b];
-    },
-
-    '>': function(a, b) {
-      return a > b;
-    },
-
-    'instanceof': function(a, b) {
-      return a instanceof b;
-    },
-
-    'regex': function(str, flags) {
-      return new RegExp(str, flags);
-    },
-
-    'if': macro(function(test, t, f) {
-      if (this["eval"](test)) {
-        return this["eval"](t);
-      } else {
-        return this["eval"](f);
+    }).call(this);
+    ctx = this;
+    f = function() {
+      var ctx2, j, len, ref, stat, vals;
+      vals = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+      ctx2 = new exports.Context(ctx.vars);
+      for (i = j = 0, len = args.length; j < len; i = ++j) {
+        arg = args[i];
+        ctx2.vars[arg] = (ref = vals[i]) != null ? ref : defaults[i];
       }
-    }),
-
-    'loop': macro(function() {
-      var arg, args, e, fn, stats, vals;
-      args = arguments[0], stats = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-      fn = this["eval"](['fn', args].concat(slice.call(stats)));
       vals = (function() {
-        var j, len, ref, results;
-        ref = fn.parameters || [];
+        var len1, n, results;
         results = [];
-        for (j = 0, len = ref.length; j < len; j++) {
-          arg = ref[j];
-          results.push(this.vars[arg]);
+        for (n = 0, len1 = stats.length; n < len1; n++) {
+          stat = stats[n];
+          results.push(ctx2["eval"](stat));
         }
         return results;
-      }).call(this);
-      while (true) {
-        try {
-          return fn.apply(null, vals);
-        } catch (e) {
-          if (!(e instanceof FlowControl)) {
-            throw e;
-          }
-          vals = e.args;
-        }
+      })();
+      return vals[vals.length - 1];
+    };
+    f.parameters = args;
+    return f;
+  });
+
+  host.do = function(fn) {
+    var arg;
+    return fn.apply(null, (function() {
+      var j, len, ref, results;
+      ref = fn.parameters || [];
+      results = [];
+      for (j = 0, len = ref.length; j < len; j++) {
+        arg = ref[j];
+        results.push(this.vals[arg]);
       }
-    }),
+      return results;
+    }).call(this));
+  };
 
-    'continue': function() {
-      throw new FlowControl('continue', [].slice.call(arguments));
-    },
-
-    'combine': function() {
-      var expr, exprs, j, k, len, ret, v;
-      exprs = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-      ret = {};
-      for (j = 0, len = exprs.length; j < len; j++) {
-        expr = exprs[j];
-        for (k in expr) {
-          v = expr[k];
-          ret[k] = v;
-        }
-      }
-      return ret;
-    },
-
-    'map': function(f, expr) {
-      var i, j, len, results, results1, v;
-      if (!Array.isArray(expr)) {
-        results = [];
-        for (i in expr) {
-          v = expr[i];
-          results.push(f(v, i));
-        }
-        return results;
-      } else {
-        results1 = [];
-        for (i = j = 0, len = expr.length; j < len; i = ++j) {
-          v = expr[i];
-          results1.push(f(v, i));
-        }
-        return results1;
-      }
-    },
-
-    'reduce': function(f, expr) {
-      var i, j, len, results, results1, v;
-      if (!Array.isArray(expr)) {
-        results = [];
-        for (i in expr) {
-          v = expr[i];
-          if (f(v, i)) {
-            results.push([v, i]);
-          }
-        }
-        return results;
-      } else {
-        results1 = [];
-        for (i = j = 0, len = expr.length; j < len; i = ++j) {
-          v = expr[i];
-          if (f(v, i)) {
-            results1.push(v);
-          }
-        }
-        return results1;
-      }
-    },
-
-    'print': function() {
+  host.macro = macro(function() {
+    var args, f, m, stats;
+    args = arguments[0], stats = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    f = this["eval"](['fn', args].concat(slice.call(stats)));
+    m = macro(function() {
       var args;
       args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-      return console.log.apply(console, args);
-    },
-
-    'throw': function (expr) {
-      throw expr;
-    },
-
-    'Error': Error,
-
-    'host': global,
+      return this["eval"](f.apply(null, args));
+    });
+    return m;
   });
+
+  host.call = function() {
+    var args, meth, obj;
+    obj = arguments[0], meth = arguments[1], args = 3 <= arguments.length ? slice.call(arguments, 2) : [];
+    return obj[meth].apply(obj, args);
+  };
+
+  host.quote = macro(function(arg) {
+    return arg;
+  });
+
+  host['atom?'] = macro(function(arg) {
+    return typeof arg === 'string';
+  });
+
+  host.list = function() {
+    var args;
+    args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+    return args;
+  };
+
+  host.pairs = function(obj) {
+    var k, results, v;
+    results = [];
+    for (k in obj) {
+      v = obj[k];
+      results.push([k, v]);
+    }
+    return results;
+  };
+
+  host.first = function(list) {
+    return list != null ? list[0] : void 0;
+  };
+
+  host.rest = function(list) {
+    return list != null ? list.slice(1) : void 0;
+  };
+
+  host.concat = function() {
+    var args, list;
+    list = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    return list.concat(args);
+  };
+
+  host['empty?'] = function(list) {
+    return !(list != null ? list.length : void 0);
+  };
+
+  host.new = function() {
+    var Obj, args;
+    Obj = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    return (function(func, args, ctor) {
+      ctor.prototype = func.prototype;
+      var child = new ctor, result = func.apply(child, args);
+      return Object(result) === result ? result : child;
+    })(Obj, args, function(){});
+  };
+
+  host['='] = macro(function(left, v) {
+    var assign, tryAssign;
+    assign = (function(_this) {
+      return function(name, val) {
+        if (hasOwnProperty(_this.vars, name)) {
+          throw new Error('Cannot reassign variable in this scope: ' + name);
+        }
+        return _this.vars[name] = val;
+      };
+    })(this);
+    tryAssign = (function(_this) {
+      return function(left, v) {
+        var i, j, len, ref, results, x;
+        if (Array.isArray(left) && left[0] === 'list') {
+          ref = left.slice(1);
+          results = [];
+          for (i = j = 0, len = ref.length; j < len; i = ++j) {
+            x = ref[i];
+            results.push(tryAssign(x, v[i]));
+          }
+          return results;
+        } else if (typeof left === 'string') {
+          return assign(left, v);
+        } else {
+          throw new Error('Invalid assignment left hand side');
+        }
+      };
+    })(this);
+    return tryAssign(left, this["eval"](v));
+  });
+
+  // Arithmetic
+
+  host['=='] = function(a, b) {
+    return a === b;
+  };
+
+  host['+'] = function (a, b) {
+    return a + b;
+  };
+
+  host['-'] = function(a, b) {
+    return a - b;
+  };
+
+  host['/'] = function(a, b) {
+    return a / b;
+  };
+
+  host['*'] = function(a, b) {
+    return a * b;
+  };
+
+  host['%'] = function(a, b) {
+    return a % b;
+  };
+
+  host['.'] = function(a, b) {
+    return a[b];
+  };
+
+  host['>'] = function(a, b) {
+    return a > b;
+  };
+
+  host['instanceof'] = function(a, b) {
+    return a instanceof b;
+  };
+
+  host['regex'] = function(str, flags) {
+    return new RegExp(str, flags);
+  };
+
+  host['if'] = macro(function(test, t, f) {
+    if (this["eval"](test)) {
+      return this["eval"](t);
+    } else {
+      return this["eval"](f);
+    }
+  });
+
+  host['loop'] = macro(function() {
+    var arg, args, e, fn, stats, vals;
+    args = arguments[0], stats = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    fn = this["eval"](['fn', args].concat(slice.call(stats)));
+    vals = (function() {
+      var j, len, ref, results;
+      ref = fn.parameters || [];
+      results = [];
+      for (j = 0, len = ref.length; j < len; j++) {
+        arg = ref[j];
+        results.push(this.vars[arg]);
+      }
+      return results;
+    }).call(this);
+    while (true) {
+      try {
+        return fn.apply(null, vals);
+      } catch (e) {
+        if (!(e instanceof FlowControl)) {
+          throw e;
+        }
+        vals = e.args;
+      }
+    }
+  });
+
+  host['continue'] = function() {
+    throw new FlowControl('continue', [].slice.call(arguments));
+  };
+
+  host['combine'] = function() {
+    var expr, exprs, j, k, len, ret, v;
+    exprs = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+    ret = {};
+    for (j = 0, len = exprs.length; j < len; j++) {
+      expr = exprs[j];
+      for (k in expr) {
+        v = expr[k];
+        ret[k] = v;
+      }
+    }
+    return ret;
+  },
+
+  host['map'] = function(f, expr) {
+    var i, j, len, results, results1, v;
+    if (!Array.isArray(expr)) {
+      results = [];
+      for (i in expr) {
+        v = expr[i];
+        results.push(f(v, i));
+      }
+      return results;
+    } else {
+      results1 = [];
+      for (i = j = 0, len = expr.length; j < len; i = ++j) {
+        v = expr[i];
+        results1.push(f(v, i));
+      }
+      return results1;
+    }
+  };
+
+  host['reduce'] = function(f, expr) {
+    var i, j, len, results, results1, v;
+    if (!Array.isArray(expr)) {
+      results = [];
+      for (i in expr) {
+        v = expr[i];
+        if (f(v, i)) {
+          results.push([v, i]);
+        }
+      }
+      return results;
+    } else {
+      results1 = [];
+      for (i = j = 0, len = expr.length; j < len; i = ++j) {
+        v = expr[i];
+        if (f(v, i)) {
+          results1.push(v);
+        }
+      }
+      return results1;
+    }
+  };
+
+  host['print'] = function() {
+    var args;
+    args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+    return console.log.apply(console, args);
+  };
+
+  host['throw'] = function (expr) {
+    throw expr;
+  };
+
+  return new exports.Context(host);
 };
 
 var eval = function(code, ctx) {
